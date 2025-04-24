c24d7e4ffbe529034ddc80d7553053c7b9664736
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2025-04-23 23:41:19 -0300

    intento de deploy

--- sala-de-juegos/src/app/pages/preguntados/preguntados.component.ts
+++ sala-de-juegos/src/app/pages/preguntados/preguntados.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { AuthService } from '../../services/auth.service';
 import { DatabaseService } from '../../services/database.service';
 import { NgIf, NgFor, NgClass } from '@angular/common';
@@ -12,31 +12,30 @@ import { NgIf, NgFor, NgClass } from '@angular/common';
   styleUrls: ['./preguntados.component.css']
 })
 
-export class PreguntadosComponent implements OnInit {
+export class PreguntadosComponent implements OnInit
+{
   auth = inject(AuthService);
   db = inject(DatabaseService);
   http = inject(HttpClient);
   cdr = inject(ChangeDetectorRef);
-
   preguntas: any[] = [];
   indice: number = 0;
   aciertos: number = 0;
   juegoFinalizado: boolean = false;
   gano: boolean = false;
-
   tiempoInicio: number = 0;
   tiempoTranscurrido: number = 0;
   tiempoFormateado: string = '00:00';
   intervalo: any = null;
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.cargarPreguntas();
   }
 
-  cargarPreguntas(): void {
-    const url = 'https://the-trivia-api.com/api/questions?limit=20&difficulty=easy';
-  
-    this.http.get<any[]>(url).subscribe(res => {
+  cargarPreguntas(): void
+  {
+    this.http.get<any[]>('https://the-trivia-api.com/api/questions?limit=20&difficulty=easy').subscribe(res => {
       this.preguntas = res.map(p => ({
         question: this.decodeHtml(p.question),
         correct_answer: this.decodeHtml(p.correctAnswer),
@@ -45,33 +44,39 @@ export class PreguntadosComponent implements OnInit {
           ...p.incorrectAnswers.map((r: string) => this.decodeHtml(r))
         ])
       }));
-      console.log('Preguntas cargadas:', this.preguntas);
+      // console.log('Preguntas cargadas:', this.preguntas);
       this.cdr.detectChanges();
     });
     this.tiempoInicio = Date.now();
     this.iniciarTemporizador();
   }
-  
 
-  responder(opcion: string): void {
+  responder(opcion: string): void
+  {
     if (this.juegoFinalizado) return;
   
     const actual = this.preguntas[this.indice];
   
-    if (opcion === actual.correct_answer) {
+    if (opcion === actual.correct_answer)
+    {
       this.aciertos++;
-      if (this.aciertos >= 6) {
-        this.finalizarJuego(true); // Ganó con 6 aciertos
-      } else {
-        this.indice++; // Avanza a la siguiente pregunta solo si sigue jugando
+      if (this.aciertos > 5)
+      {
+        this.finalizarJuego(true);
       }
-    } else {
-      this.finalizarJuego(false); // Se equivocó => pierde
+      else
+      {
+        this.indice++;
+      }
+    }
+    else
+    {
+      this.finalizarJuego(false);
     }
   }
-  
 
-  async finalizarJuego(gano: boolean) {
+  async finalizarJuego(gano: boolean)
+  {
     this.juegoFinalizado = true;
     this.gano = gano;
 
@@ -97,15 +102,18 @@ export class PreguntadosComponent implements OnInit {
     clearInterval(this.intervalo);
   }
 
-  shuffle(array: string[]): string[] {
+  shuffle(array: string[]): string[]
+  {
     return array.sort(() => Math.random() - 0.5);
   }
 
-  decodeHtml(html: string): string {
+  decodeHtml(html: string): string
+  {
     const txt = document.createElement("textarea");
     txt.innerHTML = html;
     return txt.value;
   }
+
   iniciarTemporizador(): void
   {
     if (this.intervalo)
@@ -119,6 +127,7 @@ export class PreguntadosComponent implements OnInit {
       this.cdr.detectChanges();
     }, 1000);
   }
+
   formatearTiempo(segundos: number): string
   {
     const mins = Math.floor(segundos / 60).toString().padStart(2, '0');
