d8af63c7cbff2fb66c67dac6929835d6cf0ac57f
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2025-04-27 04:58:39 -0300

    login y registro completos

--- sala-de-juegos/src/app/pages/registro/registro.component.ts
+++ sala-de-juegos/src/app/pages/registro/registro.component.ts
@@ -1,9 +1,8 @@
-import { Component, inject, InjectionToken, OnInit, signal} from '@angular/core';
+import { Component, inject, OnInit, signal} from '@angular/core';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Usuario } from '../../classes/Usuario';
 import { AuthService } from '../../services/auth.service';
 import { DatabaseService } from '../../services/database.service';
-import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -20,15 +19,16 @@ export class RegistroComponent implements OnInit
   db = inject(DatabaseService);
   registro = signal<any | null>(null);
   mensajeLogin = signal<any | null>(null);
+  mostrarClave: boolean = false;
 
   ngOnInit()
   {
     this.formulario = new FormGroup({
-      correo: new FormControl('', {validators: [Validators.required, Validators.email]}),
+      correo: new FormControl('', {validators: [Validators.required, Validators.email, Validators.maxLength(50)]}),
       nombre: new FormControl('', {validators: [Validators.minLength(3), Validators.maxLength(15), Validators.required]}),
-      apellido: new FormControl('', {validators: [Validators.minLength(3), Validators.maxLength(15), Validators.required]}),
-      edad: new FormControl('', {validators: [Validators.min(1), Validators.max(115), Validators.required]}),
-      clave: new FormControl('', {validators: [Validators.required, Validators.minLength(6)]})
+      apellido: new FormControl('', {validators: [Validators.minLength(3), Validators.maxLength(25), Validators.required]}),
+      edad: new FormControl('', {validators: [Validators.min(4), Validators.max(115), Validators.required]}),
+      clave: new FormControl('', {validators: [Validators.required, Validators.minLength(6), Validators.maxLength(20)]})
     });
   }
 
@@ -36,34 +36,29 @@ export class RegistroComponent implements OnInit
   {
     if (!this.formulario?.valid)
     {
-      return this.registro.set('form invalido');
+      return this.registro.set('Error, formulario inválido');
     }
-    else
+
+    const { data: usuarios, error: errorUsuario } = await this.auth.supabase.from("Usuarios").select("correo").eq("correo", this.correo?.value);
+    if (usuarios?.length === 1) //valido si ya existe el mail
     {
-      //valido si ya existe el mail
-      const { data: usuarios, error: errorUsuario } = await this.auth.supabase.from("Usuarios").select("correo").eq("correo", this.correo?.value);
-      if (usuarios?.length === 1)
-      {
-        return this.registro.set('usuario registrado');
-      }
-      else
-      {
-        //sino existe, creo la cuenta
-        let uid = await this.auth.crearCuenta(this.correo?.value, this.clave?.value);
-        if (uid === -1)
-        {
-          return this.registro.set('Error, mail inexistente!');
-        }
-        this.usuario = new Usuario(this.correo?.value, this.nombre?.value, this.apellido?.value, this.edad?.value, uid);
-        this.db.insertarUsuario(this.usuario);
-        this.registro.set('registro exitoso');
-        this.auth.iniciarSesion(this.correo?.value, this.clave?.value);
-        setTimeout(() => {
-          this.auth.router.navigateByUrl('/bienvenida');
-        }, 2000); //espero 2 segundos para que se vea el mensaje
-        return;
-      }
+      return this.registro.set('Error, usuario ya registrado');
     }
+
+    let uid = await this.auth.crearCuenta(this.correo?.value, this.clave?.value);
+    this.usuario = new Usuario(this.correo?.value, this.nombre?.value, this.apellido?.value, this.edad?.value, uid);
+    this.db.insertarUsuario(this.usuario);
+    this.registro.set('registro exitoso');
+    this.auth.iniciarSesion(this.correo?.value, this.clave?.value);
+    setTimeout(() => {
+      this.auth.router.navigateByUrl('/bienvenida');
+    }, 2000);
+    return;
+  }
+
+  verClave()
+  {
+    this.mostrarClave = !this.mostrarClave;
   }
 
   get correo() {return this.formulario?.get('correo');}
