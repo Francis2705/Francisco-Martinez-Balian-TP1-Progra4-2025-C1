ca845af7f8b7768f32f02a15fc011556dd73d4e4
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2025-04-27 20:00:50 -0300

    falta todo de sala de chat y los 4 juegos

--- sala-de-juegos/src/app/pages/sala-de-chat/sala-de-chat.component.ts
+++ sala-de-juegos/src/app/pages/sala-de-chat/sala-de-chat.component.ts
@@ -1,10 +1,9 @@
 import { NgClass, NgFor } from '@angular/common';
-import { ChangeDetectorRef, Component, inject, NgModule, NgZone, OnChanges, signal, SimpleChanges } from '@angular/core';
-import { FormsModule, NgModel } from '@angular/forms';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { DatabaseService } from '../../services/database.service';
-import { Router, RouterLink } from '@angular/router';
-import { Usuario } from '../../classes/Usuario';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sala-de-chat',
@@ -16,31 +15,39 @@ export class SalaDeChatComponent
 {
   auth = inject(AuthService);
   db = inject(DatabaseService);
-  // cdr = inject(ChangeDetectorRef);
-  // zone = inject(NgZone);
-
-  mensajes = signal<any[]>([]);  // Mensajes recibidos
-  nuevoMensaje: string = '';  // Mensaje que el usuario va a enviar
-  usuarioActual?: any;  // Usuario actual
+  mensajes = signal<any[]>([]);
+  nuevoMensaje: string = '';
+  usuarioActual?: any;
+  usuarioClase?: any;
+  nombreUsuario = signal<any[]>([]);
 
   ngOnInit()
   {
-    // Cargar los mensajes y escuchar los nuevos
     this.db.obtenerMensajes().then(mensajes => {
       this.mensajes.set(mensajes);
     });
-    
-    // Suscribirse a nuevos mensajes en tiempo real
-    this.db.suscribirAEventosMensajes().subscribe((mensajeNuevo : any) => {
 
-      
+    this.db.suscribirAEventosMensajes().subscribe((mensajeNuevo : any) => {
       this.mensajes.update(mensajes => [...mensajes, mensajeNuevo]);
-      
     });
-    
-    // Obtener usuario actual
+
     this.usuarioActual = this.auth.user();
-    // console.log(this.usuarioActual);
+    this.setearUsuario(this.auth.user()?.id);
+  }
+
+  async setearUsuario(userId: string | undefined)
+  {
+    const { data, error } = await this.db.supabase.from("Usuarios").select("*").eq("uid", userId).single();
+
+    if (data)
+    {
+      this.usuarioClase = data;
+      this.nombreUsuario.set(this.usuarioClase?.nombre);
+    }
+    else
+    {
+      console.error("Error al obtener nombre:", error);
+    }
   }
 
   async enviarMensaje()
@@ -53,12 +60,11 @@ export class SalaDeChatComponent
         fecha: new Date(),
       };
 
-      console.log(mensaje);
       await this.db.guardarMensaje(mensaje).then(() => {
         this.nuevoMensaje = '';
       });
     }
-    
+
     setTimeout(async () => {
       await this.db.obtenerMensajes().then(mensajes => {
         this.mensajes.set(mensajes);
