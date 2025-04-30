243cd55f65918c7d462d968b91854ec9c257035c
Francisco Martinez Balian <franciscomartinezbalian@gmail.com>
2025-04-30 18:10:22 -0300

    agregado de lazy loads

--- sala-de-juegos/src/app/app.routes.ts
+++ sala-de-juegos/src/app/app.routes.ts
@@ -1,29 +1,29 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { BienvenidaComponent } from './pages/bienvenida/bienvenida.component';
-import { QuienSoyComponent } from './pages/quien-soy/quien-soy.component';
-import { AhorcadoComponent } from './pages/ahorcado/ahorcado.component';
-import { MayorOMenorComponent } from './pages/mayor-o-menor/mayor-o-menor.component';
-import { SalaDeChatComponent } from './pages/sala-de-chat/sala-de-chat.component';
-import { JuegoPropioComponent } from './pages/juego-propio/juego-propio.component';
-import { PreguntadosComponent } from './pages/preguntados/preguntados.component';
 import { chequeoLoginGuard } from './guards/chequeo-login.guard';
 import { chequeoJuegosGuard } from './guards/chequeo-juegos.guard';
-import { ResultadosComponent } from './pages/resultados/resultados.component';
-import { ReglasComponent } from './pages/reglas/reglas.component';
 
 export const routes: Routes = [
-    {path: "login", component: LoginComponent, canActivate: [chequeoLoginGuard]},
-    {path: "registro", component: RegistroComponent, canActivate: [chequeoLoginGuard]},
-    {path: "bienvenida", component: BienvenidaComponent},
-    {path: "quien-soy", component: QuienSoyComponent},
-    {path: "ahorcado", component: AhorcadoComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "mayor-o-menor", component: MayorOMenorComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "sala-de-chat", component: SalaDeChatComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "juego-propio", component: JuegoPropioComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "preguntados", component: PreguntadosComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "resultados", component: ResultadosComponent, canActivate: [chequeoJuegosGuard]},
-    {path: "reglas", component: ReglasComponent, canActivate: [chequeoJuegosGuard]},
+    {path: "login", loadComponent: () => import('./pages/login/login.component')
+        .then((modulo) => modulo.LoginComponent), canActivate: [chequeoLoginGuard]},
+    {path: "registro", loadComponent: () => import('./pages/registro/registro.component')
+        .then((modulo) => modulo.RegistroComponent), canActivate: [chequeoLoginGuard]},
+    {path: "bienvenida", loadComponent: () => import('./pages/bienvenida/bienvenida.component')
+        .then((modulo) => modulo.BienvenidaComponent)},
+    {path: "quien-soy", loadComponent: () => import('./pages/quien-soy/quien-soy.component')
+        .then((modulo) => modulo.QuienSoyComponent)},
+    {path: "ahorcado", loadComponent: () => import('./pages/ahorcado/ahorcado.component')
+        .then((modulo) => modulo.AhorcadoComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "mayor-o-menor", loadComponent: () => import('./pages/mayor-o-menor/mayor-o-menor.component')
+        .then((modulo) => modulo.MayorOMenorComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "sala-de-chat", loadComponent: () => import('./pages/sala-de-chat/sala-de-chat.component')
+        .then((modulo) => modulo.SalaDeChatComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "juego-propio", loadComponent: () => import('./pages/juego-propio/juego-propio.component')
+        .then((modulo) => modulo.JuegoPropioComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "preguntados", loadComponent: () => import('./pages/preguntados/preguntados.component')
+        .then((modulo) => modulo.PreguntadosComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "resultados", loadComponent: () => import('./pages/resultados/resultados.component')
+        .then((modulo) => modulo.ResultadosComponent), canActivate: [chequeoJuegosGuard]},
+    {path: "reglas", loadComponent: () => import('./pages/reglas/reglas.component')
+        .then((modulo) => modulo.ReglasComponent), canActivate: [chequeoJuegosGuard]},
     {path: "", redirectTo: "bienvenida", pathMatch: "full"},
 ];
